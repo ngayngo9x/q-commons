36070e3211399f1e7871884e78639d0bc6866e19
ngayngo9x <nguyentruongquy@gmail.com>
2016-04-04 21:29:42 +0700

    init

---
+++ src/main/java/com/pheu/common/PropertyLoader.java
@@ -0,0 +1,30 @@
+package com.pheu.common;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+public class PropertyLoader {
+
+	private PropertiesConfiguration properties;
+	
+	public PropertyLoader(String rootSource, String filename) {
+		try {
+			properties = new PropertiesConfiguration(rootSource + "/" + filename);
+		} catch (ConfigurationException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+	}
+	
+	public int getInt(String key) {
+		return properties.getInt(key);
+	}
+	
+	public float getFloat(String key) {
+		return properties.getFloat(key);
+	}
+	
+	public String getString(String key) {
+		return properties.getString(key);
+	}
+	
+}
